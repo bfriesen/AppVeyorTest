5f2088d542962384933f1915d3593b09fe880115
Brian Friesen <brianfriesen@quickenloans.com>
2018-02-27 10:13:07 -0500

    Add solutions and appveyor yml files

---
+++ BarTests/GarplyTests.cs
@@ -0,0 +1,15 @@
+using Bar;
+using System;
+using Xunit;
+
+namespace BarTests
+{
+    public class GarplyTests
+    {
+        [Fact]
+        public void CanInstantiateGarply()
+        {
+            var garply = new Garply();
+        }
+    }
+}
---
+++ FooTests/GraultTests.cs
@@ -0,0 +1,14 @@
+using Foo;
+using Xunit;
+
+namespace FooTests
+{
+    public class GraultTests
+    {
+        [Fact]
+        public void CanInstantiateGrault()
+        {
+            var grault = new Grault();
+        }
+    }
+}
